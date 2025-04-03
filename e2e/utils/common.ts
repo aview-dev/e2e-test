5a89424cd3564fc4ff4369ce632d707bfe461506
Junyeong Mun <junyeong.mun@corelinesoft.com>
2025-04-03 16:28:54 +0900

    navigate, env 관련 common 로직 추가

--- e2e/utils/common.ts
+++ e2e/utils/common.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 // .env 파일 로드
 dotenv.config();
 
+// ENV 변수 설정
 const ADMIN_ID = process.env.ADMIN_ID;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
-export { ADMIN_ID, ADMIN_PASSWORD };
+const API_ENDPOINT = process.env.API_ENDPOINT;
+
+export { ADMIN_ID, ADMIN_PASSWORD, API_ENDPOINT };
--- e2e/utils/navigate.utils.ts
+++ e2e/utils/navigate.utils.ts
@@ -1,11 +1,26 @@
 import { Page } from "@playwright/test";
-
-const endpoint = 'http://localhost';
+import { API_ENDPOINT } from "./common";
 
 export async function Login(page: Page) {
-  await page.goto(`${endpoint}/login`);
+  await page.goto(`${API_ENDPOINT}/login`);
 }
 
-export async function Mgmt(page: Page) {
-  await page.goto(`${endpoint}/mgmt`);
+export async function ConfigPage(page: Page) {
+    await page.goto(`${API_ENDPOINT}/mgmt/system/config`);
+    await page.waitForLoadState('networkidle');
+}
+
+export async function UserPage(page: Page) {
+    await page.goto(`${API_ENDPOINT}/mgmt/resource/users`);
+    await page.waitForLoadState('networkidle');
+}
+
+export async function DatastoragePage(page: Page) {
+    await page.goto(`${API_ENDPOINT}/mgmt/resource/storages`);
+    await page.waitForLoadState('networkidle');
+}
+
+export async function ApsRulePage(page: Page) {
+    await page.goto(`${API_ENDPOINT}/mgmt/resource/aps-rules`);
+    await page.waitForLoadState('networkidle');
 }
